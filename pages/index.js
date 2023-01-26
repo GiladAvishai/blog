27b1ee50ce6ddcdfba72fae242ceb21c5875aa85
Gilad Avishai <giladavishai42@gmail.com>
2023-01-26 21:39:36 +0200

    added a button to load posts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/posts");
+    const data = await response.json(); 
+    setPosts(data);
+  };
+
   return (
     <>
       <div>
         <h1>hello</h1>
+        <button onClick={fetchPosts}>load Posts</button>
+        {
+          posts.map( post => {
+            return (
+              <div key={post.id}>
+                {post.id} {post.title} {post.content}
+              </div>
+            );
+          })
+        }
       </div>
     </>
   );
