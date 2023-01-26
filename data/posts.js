9ce7908592c25524642549f2629b1eda7d1c8c3f
Gilad Avishai <giladavishai42@gmail.com>
2023-01-26 21:18:27 +0200

    created a data dir to hold fake post data

---
+++ data/posts.js
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+        id: 1,
+        title: "post",
+        content: "this is a post"
+    },
+    {
+        id: 2,
+        title: "post-2",
+        content: "this is a post-2"
+    },
+    {
+        id: 3,
+        title: "post-3",
+        content: "this is a post-3"
+    }
+];
