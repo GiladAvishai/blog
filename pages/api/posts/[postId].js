e1623fb20ea109cf12f94089e87368d6bb17b1cf
Gilad Avishai <giladavishai42@gmail.com>
2023-07-22 14:49:15 +0300

    adedd if statement in case of 404 error.

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -4,5 +4,10 @@ export default function handler(req, res) {
     const {postId} = req.query;
     const reqPost = Number({postId}.postId);
     const foundPost = posts.find((post) => post.id == reqPost);
-    res.status(200).json(foundPost);
+    if (foundPost) {
+        res.status(200).json(foundPost);
+    } else {
+        res.status(404).send('404');
+    }
+    
 }
