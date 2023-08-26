2f14dd5af9cf5a3baef72d6403b19ca575fdce3c
Gilad Avishai <giladavishai42@gmail.com>
2023-08-26 14:54:06 +0300

    added option to request specific post by Id

--- pages/api/posts/getReq/getSpecPost.js
+++ pages/api/posts/getReq/getSpecPost.js
@@ -1,21 +1,22 @@
 import { posts } from "@/data/posts";
 
-/* 
-* This function handles the req of a specific post.
-* @const postId gets the Num/Id of the post.
-* then using the Number function to change the Id to a Number type.
-* @const foundPost looks for specific post using find method. 
-* using the if statment where, if post found then  the return value is json of the post
-* and if not found then return value is 404 status.
-*/
+/*
+ * This function handles the req of a specific post.
+ * @const postId gets the Num/Id of the post.
+ * then using the Number function to change the Id to a Number type.
+ * @const foundPost looks for specific post using find method.
+ * using the if statment where, if post found then  the return value is json of the post
+ * and if not found then return value is 404 status.
+ */
 export default function handler(req, res) {
-    const {postId} = req.body.postId;
-    const reqPost = Number({postId}.postId);
-    const foundPost = posts.find((post) => post.id == reqPost);
-    if (foundPost) {
-        res.status(200).json(foundPost);
-    } else {
-        res.status(404).send('404');
-    }
-    
+  const clientInput = req.body.clientInput;
+
+  const reqPostId = Number(clientInput);
+
+  const foundPost = posts.find((post) => post.id == reqPostId);
+  if (foundPost) {
+    res.status(200).json({data: foundPost});
+  } else {
+    res.status(400).json({data: "post-Id does not exist"});
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,51 @@ import { useState } from "react";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const [reqPost, setReqPost] = useState({});
 
-  /* @function fetchPosts 
-  * an async funtion that fetch all the post through the api.
-  */
+  /* @function fetchPosts
+   * an async funtion that fetch all the post through the api.
+   */
   const fetchPosts = async () => {
     const response = await fetch("/api/posts/getReq/getPosts");
     const data = await response.json();
     setPosts(data);
+  };
+
+  const handleSubmit = async (e) => {
+    // // Stop the form from submitting and refreshing the page.
+    e.preventDefault();
+
+    // Get data from the form.
+    const data = {
+      clientInput: e.target.postIdSearch.value,
+    };
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+
+    // API endpoint where we send form data.
+    const endPoint = "/api/posts/getReq/getSpecPost";
+
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON File.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    const response = await fetch(endPoint, options);
+    const result = await response.json();
+
+    if (response.status === 400) {
+      return alert(result.data);
+    }
+
+    setReqPost((reqPost) => result.data);
   };
 
   return (
@@ -25,6 +62,18 @@ export default function Home() {
             </div>
           );
         })}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="postIdSearch">Find by post id:</label>
+            <input type="text" id="postIdSearch" name="postIdSearch" />
+            <button type="submit">Submit</button>
+          </form>
+          <div>
+            <h1>{reqPost.id}</h1>
+            <h1>{reqPost.title}</h1>
+            <h1>{reqPost.content}</h1>
+          </div>
+        </div>
       </div>
     </>
   );
